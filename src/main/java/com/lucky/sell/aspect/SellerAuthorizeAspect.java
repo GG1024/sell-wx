aac774c98bbe8bdf40431b6ff11f63b09bbb0719
oyxg1024_pro <oyxg1024@163.com>
2020-12-31 16:56:53 +0800

    修改切面指向包名

--- src/main/java/com/lucky/sell/aspect/SellerAuthorizeAspect.java
+++ src/main/java/com/lucky/sell/aspect/SellerAuthorizeAspect.java
@@ -33,8 +33,8 @@ public class SellerAuthorizeAspect {
     @Autowired
     private StringRedisTemplate redisTemplate;
 
-    @Pointcut("execution(public * com.imooc.controller.Seller*.*(..))" +
-            "&& !execution(public * com.imooc.controller.SellerUserController.*(..))")
+    @Pointcut("execution(public * com.lucky.sell.controller.Seller*.*(..))" +
+            "&& !execution(public * com.lucky.sell.controller.SellerUserController.*(..))")
     public void verify() {
     }
 
